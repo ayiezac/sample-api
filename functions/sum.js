9fe9f1421c82669ff25ef6b425821d0d447e5d69
ayiezac <afacebu.ayie@gmail.com>
2025-06-19 10:07:19 +0800

    modified code

--- functions/sum.js
+++ functions/sum.js
@@ -1,19 +1,46 @@
 export default async (req, ctx) => {
-  if (req.method === "POST") {
-    const res = ctx.json({ message: 'you posted!' });
-
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    res.headers.append("Access-Control-Allow-Headers", "*");
-    res.headers.append("Access-Control-Allow-Methods", "*");
-
-    return res;
-  } else if (req.method === "OPTIONS") {
-    const res = new Response();
-
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    res.headers.append("Access-Control-Allow-Headers", "*");
-    res.headers.append("Access-Control-Allow-Methods", "*");
-
-    return res;
+  if (req.method !== "POST") {
+    return ctx.json({ error: "Method not allowed" }, { status: 405 });
   }
+
+  try {
+    const data = JSON.parse(await req.body());
+    console.log("Received data:", data);
+    return {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify({ message: "Data received successfully", data }),
+    }
+  } catch (error) {
+    return{
+      status: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify({ error: error.message, message: "Invalid req body" }),
+    }
+  }
+  // if (req.method === "POST") {
+  //   const res = ctx.json({ message: 'you posted!' });
+
+  //   res.headers.set("Access-Control-Allow-Origin", "*");
+  //   res.headers.append("Access-Control-Allow-Headers", "*");
+  //   res.headers.append("Access-Control-Allow-Methods", "*");
+
+  //   return res;
+  // } else if (req.method === "OPTIONS") {
+  //   const res = new Response();
+
+  //   res.headers.set("Access-Control-Allow-Origin", "*");
+  //   res.headers.append("Access-Control-Allow-Headers", "*");
+  //   res.headers.append("Access-Control-Allow-Methods", "*");
+
+  //   return res;
+  // }
 };
